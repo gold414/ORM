61a8f548f97e7e056cb449ab0862deba1505aedd
Thangapazham <thangapazham@340gmail.com>
2025-09-27 08:05:55 +0530

    Success

--- sparrow/ormapp/models.py
+++ sparrow/ormapp/models.py
@@ -5,5 +5,6 @@ class Car_DB(models.Model):
            Car_model = models.CharField(max_length = 10)
            Car_type  = models.CharField(max_length = 8)
            VIN_no    = models.CharField(max_length = 17,primary_key = True)
+           Seat_count = models.IntegerField()
 class DBAdmin(admin.ModelAdmin):
-    list_display = ["Car_brand","Car_model","Car_type","VIN_no"]
+    list_display = ["Car_brand","Car_model","Car_type","VIN_no","Seat_count"]
